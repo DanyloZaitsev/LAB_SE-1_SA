0f3efea71e425e6469a178f141e4a3183ef5579a
DanyloZaitsev <danilzaytsevvvvv@gmail.com>
2022-10-02 21:01:01 +0300

    001

---
+++ Project1.cpp
@@ -0,0 +1,33 @@
+﻿#include "stdafx.h"
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "Enter size your array(more than 4, but less than 48):" << endl;
+	cin >> n;
+	int* arr = new int[n];
+	int i;
+	if (n > 4)
+	{                                                       
+		cout << "Your array from the Fibonacci series: ";   
+		cout << "1 1 ";                                     
+		arr[1] = 1;
+		arr[2] = 1;
+		for (i = 3; i < (n - 1); ++i)
+		{
+			arr[i] = arr[i - 1] + arr[i - 2];
+			cout << arr[i] << " ";
+		}
+	}
+	else
+	{
+		cout << "Warning! Your array can't be so small" << endl; 
+	}
+
+	cout << endl;
+	system("pause");
+	return 0;
+}
